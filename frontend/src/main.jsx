7dcf8ee1220f073934dd6ad117e344dd4cacd44b
hkmoff <khodameyth@gmail.com>
2026-09-15 10:37:25 +0100

    Add files via upload

---
+++ frontend/src/main.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
+import Login from "./pages/Login.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Products from "./pages/Products.jsx";
+import Settings from "./pages/Settings.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/dashboard/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard/products" element={<Products />} />
+        <Route path="/dashboard/settings" element={<Settings />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
